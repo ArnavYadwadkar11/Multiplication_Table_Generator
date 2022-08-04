a2c374a2428d794a1c4ee340b82aeee12ffca7da
ArnavYadwadkar11 <109950029+ArnavYadwadkar11@users.noreply.github.com>
2022-08-04 14:31:22 +0530

    Add files via upload

---
+++ Table_Generator.py
@@ -0,0 +1,4 @@
+print("This is a program to generate the multiplication tables")
+n = int(input('Enter the number for printing out its table: '))
+for i in range (1,11):
+    print(n, " x ", i, " = ", i*n)
